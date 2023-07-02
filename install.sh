8386acdde42b822e81a2782ea9ae6cd7d22f47d2
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2023-07-01 20:55:25 -0400

    Shell Script Install

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+sudo zypper install android-tools -y;
+
+CURRENT_UID="$(id -u)"
+
+if [ "$CURRENT_UID" -ne 0 ]; then
+  echo "Please run as root!"
+  exit 1
+fi
+
+if [ "$USE_SYMLINK" = "true" ]; then
+  ln -sf "$PWD"/51-android.rules /etc/udev/rules.d/51-android.rules
+else
+  cp -v 51-android.rules /etc/udev/rules.d/51-android.rules
+fi
+
+chmod a+r /etc/udev/rules.d/51-android.rules
+
+if [ "$USE_GROUP" = "false" ]; then
+  install -Dm644 android-udev.conf \
+    /usr/lib/sysusers.d/android-udev.conf
+  systemd-sysusers
+else
+  getent group 'adbusers' >/dev/null ||
+    groupadd -f 'adbusers'
+fi
+
+usermod -a -G adbusers "$(logname)"
+udevadm control --reload-rules
+
+if [ "$USE_SERVICE_CMD" = "true" ]; then
+  service udev restart
+else
+  systemctl restart systemd-udevd.service
+fi
+Footer
